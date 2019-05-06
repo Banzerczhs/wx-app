8fbefde51ea3528c8ec69e016f2ca7aed89946da
Yahooo <chinazhzhk@163.com>
2019-05-06 16:03:34 +0800

    19年5月6日第二次修改

---
+++ src/listdata/indexList.js
@@ -0,0 +1,78 @@
+let indexlist = {
+    indexnav:        
+    [   //indexnav
+        {
+            src:"/assets/img/Building/x_0042_失物招领-4.png",
+            class:"x_0042",
+            name:'失物招领'
+        },
+        {
+            src:"/assets/img/Building/x_0038_图书馆21.png",
+            class:"x_0038",
+            name:'图书馆'
+        },
+        {
+            src:"/assets/img/Building/x_0037_教学楼2.png",
+            class:"x_0037",
+            name:'教学楼'
+        },
+        {
+            src:"/assets/img/Building/x_0039_社团.png",
+            class:"x_0039",
+            name:'社团'
+        },
+        {
+            src:"/assets/img/Building/x_0040_宿舍.png",
+            class:"x_0040",
+            name:'宿舍'
+        },
+        {
+            src:"/assets/img/Building/x_0041_点歌台-6.png",
+            class:"x_0041",
+            name:'点歌台'
+        },
+        {
+            src:"/assets/img/Building/x_0036_网络中心.png",
+            class:"x_0036",
+            name:'网络中心'
+        }
+    ]
+}
+let fish ={
+    fishtop:[
+        {
+            src:"/assets/img/Bubble/x_0011_气泡.png",
+        },
+        {
+            src:"/assets/img/Bubble/x_0007_气泡.png",
+        },
+        {
+            src:"/assets/img/fish/x_0010_鱼.png",
+        },
+    ],
+    fishmiddle:[
+        {
+            src:"/assets/img/Bubble/x_0011_气泡.png",
+        },
+        {
+            src:"/assets/img/Bubble/x_0007_气泡.png",
+        },
+        {
+            src:"/assets/img/fish/x_0009_鱼.png",
+        },
+    ],
+    fishbottom:[
+        {
+            src:"/assets/img/Bubble/x_0011_气泡.png",
+        },
+        {
+            src:"/assets/img/Bubble/x_0007_气泡.png",
+        },
+        {
+            src:"/assets/img/fish/x_0006_鱼.png",
+        },
+    ]
+}
+export default {
+    indexlist,fish
+};
